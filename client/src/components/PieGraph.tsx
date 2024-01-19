94371a2b913e886f37654e4ed2c8f0307a1030f7
Farhan <farhankolad@gmail.com>
2024-01-19 18:02:55 +0530

    Some Changes

--- client/src/components/PieGraph.tsx
+++ client/src/components/PieGraph.tsx
@@ -1,19 +1,24 @@
-// import { useSearchParams } from "react-router-dom";
-// import { useGetReq } from "../hooks/useGetReq";
 import { PieChart } from "@mui/x-charts";
+import { getMonth } from "../utils/getMonth";
+import { useSearchParams } from "react-router-dom";
 
 export default function PieGraph({ pieData }: { pieData: any }) {
+  const [searchParams] = useSearchParams();
+
   return (
     <div>
-      <h3>Pie Chart Statistics </h3>
+      <h3>
+        Pie Chart Statistics -{" "}
+        {getMonth(searchParams.get("selectedMonth") || 2)}
+      </h3>
       <PieChart
         series={[
           {
             data: pieData,
           },
         ]}
-        width={400}
-        height={200}
+        width={600}
+        height={300}
       />
     </div>
   );
